0973e4208cef55aa5ee6abef897f705ffe67dfc8
dark0dave <dark0dave@mykolab.com>
2026-09-13 12:35:18 +0100

    fix(lua): some lua for new races
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

---
+++ subrace/lua/m_zdksun.lua
@@ -0,0 +1,42 @@
+swap = {
+  ['placeholder'] = 2,
+  ['selected'] = 0
+}
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{'
+      for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '\n ['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. ' \n}'
+   else
+      return tostring(o)
+   end
+end
+function debugChargen()
+  print(dump(chargen))
+end
+function theOldSwitchAroooooo(race)
+  if race == %race_index% then
+    createCharScreen:OnRaceSelectButtonClick(swap.placeholder)
+  else
+    createCharScreen:OnRaceSelectButtonClick(race)
+  end
+  swap.selected = race
+end
+function revert(race)
+  if race == %race_index% then
+    createCharScreen:OnRaceSelectButtonClick(swap.selected)
+  end
+end
+function getRaceTitle()
+  if swap.selected ~= 0 then
+    local lower_race = Infinity_FetchString(chargen.races[swap.selected].name)
+    local race = lower_race:lower():gsub("^%l", string.upper)
+    local result, count = string.gsub(chargen.information, "Race: %w+", "Race: " .. race, 1)
+    return result
+  else
+    return chargen.information
+  end
+end
